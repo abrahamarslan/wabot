f034df1e5c406b5e9e501ec04b47977a0053f69a
abrahamarslan <arslan@abrahamarslan.com>
2021-04-10 23:22:19 +0530

    #2 - Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,7 @@
     </head>
     <body class="antialiased">
         <div id="app">
-            <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                <example-component></example-component>
-            </div>
+
         </div>
     <script src="{!! asset('js/app.js') !!}"></script>
     </body>
