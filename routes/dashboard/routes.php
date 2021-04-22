2ff3911604bc8dc911de4ea133d995d24ffb9098
abrahamarslan <arslan@abrahamarslan.com>
2021-04-22 18:09:01 +0530

    #32 - Route updates

--- routes/dashboard/routes.php
+++ routes/dashboard/routes.php
@@ -49,17 +49,17 @@ Route::group(array('prefix' => 'dashboard', 'middleware' => 'App\Http\Middleware
 
     //Export
     Route::group(array('prefix' => 'export'), function () {
-        Route::get('/', array('uses'=>'App\Http\Controllers\campaign\CampaignController@index', 'as'=>'campaign.index'));
-        Route::get('/create', array('uses'=>'App\Http\Controllers\campaign\CampaignController@create', 'as'=>'campaign.create'));
-        Route::post('/create', array('uses'=>'App\Http\Controllers\campaign\CampaignController@store', 'as'=>'campaign.store'));
+        Route::get('/', array('uses'=>'App\Http\Controllers\export\ExportController@index', 'as'=>'export.index'));
+        Route::get('/create', array('uses'=>'App\Http\Controllers\export\ExportController@create', 'as'=>'export.create'));
+        Route::post('/create', array('uses'=>'App\Http\Controllers\export\ExportController@store', 'as'=>'export.store'));
     });
 
     //Setting
     Route::group(array('prefix' => 'setting'), function () {
-        Route::get('/', array('uses'=>'App\Http\Controllers\campaign\CampaignController@index', 'as'=>'campaign.index'));
-        Route::get('/create', array('uses'=>'App\Http\Controllers\campaign\CampaignController@create', 'as'=>'campaign.create'));
-        Route::post('/create', array('uses'=>'App\Http\Controllers\campaign\CampaignController@store', 'as'=>'campaign.store'));
-        Route::get('/edit/{id}', array('uses'=>'App\Http\Controllers\sequence\SequenceController@update', 'as'=>'sequence.update'));
-        Route::post('/edit/{id}', array('uses'=>'App\Http\Controllers\sequence\SequenceController@postUpdate', 'as'=>'sequence.postUpdate'));
+        Route::get('/', array('uses'=>'App\Http\Controllers\setting\SettingController@index', 'as'=>'setting.index'));
+        Route::get('/create', array('uses'=>'App\Http\Controllers\setting\SettingController@create', 'as'=>'setting.create'));
+        Route::post('/create', array('uses'=>'App\Http\Controllers\setting\SettingController@store', 'as'=>'setting.store'));
+        Route::get('/edit/{id}', array('uses'=>'App\Http\Controllers\setting\SettingController@update', 'as'=>'setting.update'));
+        Route::post('/edit/{id}', array('uses'=>'App\Http\Controllers\setting\SettingController@postUpdate', 'as'=>'setting.postUpdate'));
     });
 });
