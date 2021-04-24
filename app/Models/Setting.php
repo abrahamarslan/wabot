d14de7d428eb40df0fda7aeccf98614c0598314a
abrahamarslan <arslan@abrahamarslan.com>
2021-04-24 14:42:13 +0530

    #35 - Setting Model updates

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\common\DefaultModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Setting extends Model
+class Setting extends DefaultModel
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $hidden = [];
+    public $timestamps = true;
+    /**
+     * Get table columns
+     * @return array
+     */
+    public function getTableColumns() {
+        return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+    }
+    protected $casts = [
+        'options' => 'json',
+        'setting_options' => 'json'
+    ];
 }
